d1f3f882c39dc1f520bc9e7f4bac9efb3aec9b09
VoloshinArtur <115408461+VoloshinArtur@users.noreply.github.com>
2022-10-09 16:36:50 +0300

    Add files via upload

---
+++ 111.cpp
@@ -0,0 +1,13 @@
+﻿//Voloshin AO
+//Task 1
+//09.10.2022
+#include <iostream>
+using namespace std;
+int main() {
+	int A, fd, sd;
+	cin >> A;
+	fd = A % 10;
+	sd = A / 10;
+	fd *= 10;
+	cout << fd + sd;
+}
